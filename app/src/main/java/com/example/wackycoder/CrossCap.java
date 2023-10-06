cd7ca5c95b29fa039a159b03ffa5ac85e4db29c2
naha-s1 <jnahache@ualberta.ca>
2023-10-05 21:55:22 -0600

    Revert "crosscap gets a color"
    This reverts commit d53935441c8ab69589c760830453080fcd4f0df2.

--- app/src/main/java/com/example/wackycoder/CrossCap.java
+++ app/src/main/java/com/example/wackycoder/CrossCap.java
@@ -3,8 +3,6 @@ package com.example.wackycoder;
 public class CrossCap extends Shape {
     private int z;
 
-    String color = "metallic magenta with bright transparent mauve polka-dots";
-
     public CrossCap() {
         this.z = 3;
     }
