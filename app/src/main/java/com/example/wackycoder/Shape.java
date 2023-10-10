6e0ac2577de7996da7a2e4cdb545547a1cc497ee
Krisraj Chahal <112656603+krrischahal@users.noreply.github.com>
2023-10-10 13:31:30 -0600

    Update Shape.java

--- app/src/main/java/com/example/wackycoder/Shape.java
+++ app/src/main/java/com/example/wackycoder/Shape.java
@@ -3,6 +3,8 @@ package com.example.wackycoder;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "red";
-// lab 4 
+    String color = "pink ";
+// lab 4  part 3 
+
+
 }
