308291279726aa9d2c2122306eb873f8d9ae7d32
naha-s1 <jnahache@ualberta.ca>
2023-10-09 22:26:56 -0600

    so the color was gone, I tried to put it back
    test

--- app/src/main/java/com/example/wackycoder/CrossCap.java
+++ app/src/main/java/com/example/wackycoder/CrossCap.java
@@ -3,6 +3,8 @@ package com.example.wackycoder;
 public class CrossCap extends Shape {
     private int z;
 
+    String color = "metallic magenta with bright transparent mauve polka-dots";
+
     public CrossCap() {
         this.z = 3;
     }
